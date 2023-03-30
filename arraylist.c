508048eec5cb623bf5bcb86b5d806506a45b196a
Replit user <>
2023-03-30 03:28:46 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -22,7 +22,14 @@ ArrayList *createList(void) {
 }
 
 void append(ArrayList * l, void * data){
+  if(l->size == l->capacity)
+  {
+    l->capacity *= 2;
+    l->data = (void*) realloc(l->data,l->capacity * sizeof(void));
+  }
 
+  l->data[l->size] = data;
+  l->size++;
 }
 
 void push(ArrayList * l, void * data, int i){
